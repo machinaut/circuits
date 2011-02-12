3ddfa3f50f9a053d666cbd9ddc4f62ad0efcc4b7
Alex Ray <ajray@ncsu.edu>
2011-02-12 16:50:12 -0500

    useful script to package a project up for BatchPCB submission

---
+++ util/batchpcb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# batchpcb.sh - batch up files for batchpcb
+
+if [ $# -ne 1 ]
+then
+    echo "Please supply the base filename as the only arg"
+    exit
+fi
+
+echo "tar czvf $1.tgz $1.gbl $1.gbo $1.gbs $1.gtl $1.gto $1.gtp $1.gts $1.oln $1.txt"
+tar czvf $1.tgz $1.gbl $1.gbo $1.gbs $1.gtl $1.gto $1.gtp $1.gts $1.oln $1.txt
